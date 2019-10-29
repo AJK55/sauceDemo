fdba1afaee4136d78e969cdc48866f96c4f37530
AJK55 <40635902+AJK55@users.noreply.github.com>
2019-10-29 18:56:45 -0400

    Add files via upload

--- tests/badUserTest.js
+++ tests/badUserTest.js
@@ -26,12 +26,7 @@ fixture `Bad User Tests`
       .expect(func.getPageUrl()).eql(page.baseUrl)
     await t
       .expect(page.pageTitle.innerText).eql(page.homeTitle)
-  })
-  .afterEach(async t => {
-    await t
-      .expect(func.getPageUrl()).eql(page.baseIndexUrl)
-    await t
-      .expect(page.pageTitle.innerText).eql(page.homeTitle)
+
   });
 
 
@@ -55,6 +50,29 @@ test('Problem User', async t => {
   await t
     .click(inv.menuLogout)
 
+})
 
+
+
+
+test('Problem User Image Check - Should Fail', async t => {
+  await t
+    .typeText(page.userName, creds.problem)
+  await t
+    .typeText(page.passWord, creds.passWordAll)
+  await clickLogin(t)
+  ///Need to write out but first work on good user
+  await t
+    .expect(inv.bikeLightItemImg.getAttribute('src')).eql(inv.bikeLightItemImgSrc)
+  await t
+    .expect(inv.boltTshirtItemImg.getAttribute('src')).eql(inv.boltTshirtItemImgSrc)
+  await t
+    .expect(inv.onesieItemImg.getAttribute('src')).eql(inv.onesieItemImgSrc)
+  await t
+    .expect(inv.redTshirtItemImg.getAttribute('src')).eql(inv.redTshirtItemImgSrc)
+  await t
+    .expect(inv.backpackItemImg.getAttribute('src')).eql(inv.backpackItemImgSrc)
+  await t
+    .expect(inv.fleeceJacketItemImg.getAttribute('src')).eql(inv.fleeceJacketItemImgSrc)
 
 })
